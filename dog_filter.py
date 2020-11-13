ee7b54bff061668214572f5cbc43163a86700c91
YuLin1226 <chenyl@solab.me.ntu.edu.tw>
2020-11-13 15:25:06 +0800

    update dog

--- dog_filter.py
+++ dog_filter.py
@@ -3,20 +3,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def DoG(img):
+def DoG(img, kernel_size_1 = 77, kernel_size_2 = 13):
     
     #run a 5x5 gaussian blur then a 3x3 gaussian blr
-    blur_1 = cv2.GaussianBlur(img,(15,15),0)
-    blur_2 = cv2.GaussianBlur(img,(13,13),0)
+    blur_1 = cv2.GaussianBlur(img,(kernel_size_1, kernel_size_1),0)
+    blur_2 = cv2.GaussianBlur(img,(kernel_size_2, kernel_size_2),0)
     DoGim = blur_1 - blur_2
     
-    return DoGim
+    return DoGim, blur_1, blur_2
 
 
 
 img = cv2.imread("lena.jpg",1)
 img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-img_dog = DoG(img_gray)
+img_dog, blur_1, blur_2 = DoG(img_gray)
 
-plt.imshow(img_dog, "gray")
+plt.subplot(131), plt.imshow(img_dog, "gray")
+plt.subplot(132), plt.imshow(blur_1, "gray")
+plt.subplot(133), plt.imshow(blur_2, "gray")
 plt.show()
