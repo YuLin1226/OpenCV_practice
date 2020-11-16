d26807da298b0a4f9d7c5790a171ea6230849f0b
YuLin1226 <r08522622@ntu.edu.tw>
2020-11-16 17:10:18 +0800

    test sift detector in occupancy grid map

--- pract_9.py
+++ pract_9.py
@@ -6,7 +6,8 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('lena.jpg')
+img = cv2.imread('occ.png')
+# img = cv2.imread('lena.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY) # 灰階圖
 
 siftDetector = cv2.xfeatures2d.SIFT_create()
