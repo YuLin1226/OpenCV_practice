284cdaca42cc08a2f9be8ac6d1903856c99cb827
YuLin1226 <chenyl@solab.me.ntu.edu.tw>
2021-02-25 18:12:34 +0800

    test

--- feature_detect_match.py
+++ feature_detect_match.py
@@ -69,7 +69,7 @@ def feature_detect_match(img1, img2, method="sift"):
     return img3
 
 
-def dilate(img, L=2):
+def dilate(img, L=1):
 
     img_r, img_c = np.shape(img)
     new_img = np.ones((img_r, img_c))*255
@@ -82,6 +82,20 @@ def dilate(img, L=2):
 
     return new_img.astype(np.uint8)
 
+def erode(img, L=1):
+
+    img_r, img_c = np.shape(img)
+    new_img = np.ones((img_r, img_c))*255
+    black_pts = np.where(img==0)
+    _, length = np.shape(black_pts)
+    for i in range(length):
+        x = black_pts[0][i]
+        y = black_pts[1][i]
+        if np.amax(img[x-L:x+L+1, y-L:y+L+1]) == 0:
+            new_img[x,y] = 0
+
+    return new_img.astype(np.uint8)
+
 
 
 
@@ -89,16 +103,18 @@ if __name__ == '__main__':
     img_1 = cv2.imread('Gazebo_test_map/tesst1.png')
     img_1 = cv2.cvtColor(img_1, cv2.COLOR_RGB2GRAY)
     img_1 = _transform_state(input_map=img_1)
-    img_1_ = dilate(img_1)
+    img_1_ = dilate(img_1, L=3)
+    img_1_de = erode(img_1_, L=1)
 
     img_2 = cv2.imread('Gazebo_test_map/test0223_2.png')
     img_2 = cv2.cvtColor(img_2, cv2.COLOR_RGB2GRAY) 
     img_2 = _transform_state(input_map=img_2)
-    img_2_ = dilate(img_2)
-
+    img_2_ = dilate(img_2, L=3)
+    img_2_de = erode(img_2_, L=1)
 
     img_sift = feature_detect_match(img1=img_1, img2=img_2, method="sift")
     img_dilate_sift = feature_detect_match(img1=img_1_, img2=img_2_, method="sift")
+    img_dilate_erode_sift = feature_detect_match(img1=img_1_de, img2=img_2_de, method="sift")
     # img_surf = feature_detect_match(img1=img_1, img2=img_2, method="fast")
     # img_orb = feature_detect_match(img1=img_1, img2=img_2, method="orb")
     
@@ -107,14 +123,37 @@ if __name__ == '__main__':
     # ax[0].imshow(img_sift, cmap='gray'), ax[0].set_title("sift")
     # ax[1].imshow(img_surf, cmap='gray'), ax[1].set_title("fast")
     # ax[2].imshow(img_orb, cmap='gray'), ax[2].set_title("orb")
-    
-    p1 = plt.subplot(211)
+
+    plt.figure()    
+    p1 = plt.subplot(311)
     plt.imshow(img_sift, cmap='gray')
     p1.set_title('sift')
 
-    p2 = plt.subplot(212)
+    p2 = plt.subplot(312)
     plt.imshow(img_dilate_sift, cmap='gray')
     p2.set_title('dilate + sift')
+
+    p3 = plt.subplot(313)
+    plt.imshow(img_dilate_erode_sift, cmap='gray')
+    p3.set_title('dilate + erode + sift')
+    plt.tight_layout()
+
+    plt.figure()
+    pp1 = plt.subplot(221)
+    plt.imshow(img_1_, cmap='gray')
+    pp1.set_title('dilate')
+
+    pp2 = plt.subplot(222)
+    plt.imshow(img_1_de, cmap='gray')
+    pp2.set_title('dilate + erode')
+    
+    pp3 = plt.subplot(223)
+    plt.imshow(img_2_, cmap='gray')
+    pp3.set_title('dilate')
+
+    pp4 = plt.subplot(224)
+    plt.imshow(img_2_de, cmap='gray')
+    pp4.set_title('dilate + erode')
 
     plt.tight_layout()
     plt.show()
