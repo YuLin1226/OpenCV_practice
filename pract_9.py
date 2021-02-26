2daee7d066de779e1a8d6a33ee9629930b89b1c7
YuLin1226 <chenyl@solab.me.ntu.edu.tw>
2021-02-26 12:35:22 +0800

    add bresenham to test sift

--- pract_9.py
+++ pract_9.py
@@ -6,10 +6,53 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from bresenham import bresenham
+
+
+
+def dilate(img, L=1):
+
+    img_r, img_c = np.shape(img)
+    new_img = np.ones((img_r, img_c))*255
+    black_pts = np.where(img==0)
+    _, length = np.shape(black_pts)
+    for i in range(length):
+        x = black_pts[0][i]
+        y = black_pts[1][i]
+        new_img[x-L:x+L+1, y-L:y+L+1] = 0
+
+    return new_img.astype(np.uint8)
+
+def erode(img, L=1):
+
+    img_r, img_c = np.shape(img)
+    new_img = np.ones((img_r, img_c))*255
+    black_pts = np.where(img==0)
+    _, length = np.shape(black_pts)
+    for i in range(length):
+        x = black_pts[0][i]
+        y = black_pts[1][i]
+        if np.amax(img[x-L:x+L+1, y-L:y+L+1]) == 0:
+            new_img[x,y] = 0
+
+    return new_img.astype(np.uint8)
 
 img = cv2.imread('map4.1.png')
 # img = cv2.imread('lena.jpg')
 img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY) # 灰階圖
+
+
+img_gray = np.ones((500,500))*255
+pts = list(bresenham(250,200,300,346)) + list(bresenham(250,200,100,146))
+
+for i in range(len(pts)):
+    img_gray[pts[i][0], pts[i][1]] = 0
+img_gray = dilate(img=img_gray, L=5)
+img_gray = erode(img=img_gray, L=3)
+# img_gray[200:350, 200:250] = 0
+
+img_gray = img_gray.astype(np.uint8)
+
 
 r, c = np.shape(img_gray)
 for i in range(r):
