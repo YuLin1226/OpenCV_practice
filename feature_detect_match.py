ceda15707768b92f013c8deca19e0108bf2d9029
YuLin1226 <chenyl@solab.me.ntu.edu.tw>
2021-02-26 15:25:39 +0800

    delete repeated pairs

--- feature_detect_match.py
+++ feature_detect_match.py
@@ -63,8 +63,15 @@ def feature_detect_match(img1, img2, method="sift"):
     for m, n in matches:
         if m.distance < 0.75*n.distance: #獲得的K個最佳匹配中取出來第一個和第二個，進行比值，比值小於0.75，則爲好的匹配點
             good.append([m])
+    good_new = []
+    train_list = []
+    for i in range(len(good)):
+        if good[i][0].trainIdx not in train_list:
+            good_new.append(good[i])
+            train_list.append(good[i][0].trainIdx)
+        
     img3 = np.empty((600,600))
-    img3 = cv2.drawMatchesKnn(img1, key_points_1, img2, key_points_2, good, img3, flags=2) #採用cv2.drawMatchesKnn()函數，在最佳匹配的點之間繪製直線
+    img3 = cv2.drawMatchesKnn(img1, key_points_1, img2, key_points_2, good_new, img3, flags=2) #採用cv2.drawMatchesKnn()函數，在最佳匹配的點之間繪製直線
 
     return img3
 
@@ -96,25 +103,35 @@ def erode(img, L=1):
 
     return new_img.astype(np.uint8)
 
-
+def find_contour(img1, img2):
+    
+    r,c = np.shape(img1)
+    new_img = np.ones((r,c))*255
+    for i in range(r):
+        for j in range(c):
+            if img1[i,j]!=img2[i,j]:
+                new_img[i,j]=0
+    return new_img.astype(np.uint8)
 
 
 if __name__ == '__main__':
     img_1 = cv2.imread('Gazebo_test_map/tesst1.png')
     img_1 = cv2.cvtColor(img_1, cv2.COLOR_RGB2GRAY)
     img_1 = _transform_state(input_map=img_1)
-    img_1_ = dilate(img_1, L=3)
+    img_1_ = dilate(img_1, L=6)
     img_1_de = erode(img_1_, L=1)
+    img_1_ct = find_contour(img1=img_1_, img2=dilate(img=img_1, L=4))
 
     img_2 = cv2.imread('Gazebo_test_map/test0223_2.png')
     img_2 = cv2.cvtColor(img_2, cv2.COLOR_RGB2GRAY) 
     img_2 = _transform_state(input_map=img_2)
-    img_2_ = dilate(img_2, L=3)
+    img_2_ = dilate(img_2, L=6)
     img_2_de = erode(img_2_, L=1)
+    img_2_ct = find_contour(img1=img_2_, img2=dilate(img=img_2, L=4))
 
-    img_sift = feature_detect_match(img1=img_1, img2=img_2, method="sift")
-    img_dilate_sift = feature_detect_match(img1=img_1_, img2=img_2_, method="sift")
-    img_dilate_erode_sift = feature_detect_match(img1=img_1_de, img2=img_2_de, method="sift")
+    img_sift = feature_detect_match(img1=img_1_ct, img2=img_2_ct, method="sift")
+    # img_dilate_sift = feature_detect_match(img1=img_1_, img2=img_2_, method="sift")
+    # img_dilate_erode_sift = feature_detect_match(img1=img_1_de, img2=img_2_de, method="sift")
     # img_surf = feature_detect_match(img1=img_1, img2=img_2, method="fast")
     # img_orb = feature_detect_match(img1=img_1, img2=img_2, method="orb")
     
@@ -125,35 +142,35 @@ if __name__ == '__main__':
     # ax[2].imshow(img_orb, cmap='gray'), ax[2].set_title("orb")
 
     plt.figure()    
-    p1 = plt.subplot(311)
+    # p1 = plt.subplot(311)
     plt.imshow(img_sift, cmap='gray')
-    p1.set_title('sift')
+    # p1.set_title('sift')
 
-    p2 = plt.subplot(312)
-    plt.imshow(img_dilate_sift, cmap='gray')
-    p2.set_title('dilate + sift')
+    # p2 = plt.subplot(312)
+    # plt.imshow(img_dilate_sift, cmap='gray')
+    # p2.set_title('dilate + sift')
 
-    p3 = plt.subplot(313)
-    plt.imshow(img_dilate_erode_sift, cmap='gray')
-    p3.set_title('dilate + erode + sift')
-    plt.tight_layout()
+    # p3 = plt.subplot(313)
+    # plt.imshow(img_dilate_erode_sift, cmap='gray')
+    # p3.set_title('dilate + erode + sift')
+    # plt.tight_layout()
 
-    plt.figure()
-    pp1 = plt.subplot(221)
-    plt.imshow(img_1_, cmap='gray')
-    pp1.set_title('dilate')
+    # plt.figure()
+    # pp1 = plt.subplot(221)
+    # plt.imshow(img_1_, cmap='gray')
+    # pp1.set_title('dilate')
 
-    pp2 = plt.subplot(222)
-    plt.imshow(img_1_de, cmap='gray')
-    pp2.set_title('dilate + erode')
+    # pp2 = plt.subplot(222)
+    # plt.imshow(img_1_de, cmap='gray')
+    # pp2.set_title('dilate + erode')
     
-    pp3 = plt.subplot(223)
-    plt.imshow(img_2_, cmap='gray')
-    pp3.set_title('dilate')
+    # pp3 = plt.subplot(223)
+    # plt.imshow(img_2_, cmap='gray')
+    # pp3.set_title('dilate')
 
-    pp4 = plt.subplot(224)
-    plt.imshow(img_2_de, cmap='gray')
-    pp4.set_title('dilate + erode')
+    # pp4 = plt.subplot(224)
+    # plt.imshow(img_2_de, cmap='gray')
+    # pp4.set_title('dilate + erode')
 
-    plt.tight_layout()
+    # plt.tight_layout()
     plt.show()
--- pract_9.py
+++ pract_9.py
@@ -37,18 +37,35 @@ def erode(img, L=1):
 
     return new_img.astype(np.uint8)
 
+def de(img, L=1):
+    img = dilate(img=img, L=L)
+    # img = erode(img=img, L=L)
+    return img
+
+def test(img1, img2):
+    
+    r,c = np.shape(img1)
+    new_img = np.ones((r,c))*255
+    for i in range(r):
+        for j in range(c):
+            if img1[i,j]!=img2[i,j]:
+                new_img[i,j]=0
+    return new_img
+            
+
 img = cv2.imread('map4.1.png')
 # img = cv2.imread('lena.jpg')
 img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY) # 灰階圖
 
 
 img_gray = np.ones((500,500))*255
-pts = list(bresenham(250,200,300,346)) + list(bresenham(250,200,100,146))
+pts = list(bresenham(250,200,300,416)) + list(bresenham(250,200,50,146))
+
+L = 10
 
 for i in range(len(pts)):
     img_gray[pts[i][0], pts[i][1]] = 0
-img_gray = dilate(img=img_gray, L=5)
-img_gray = erode(img=img_gray, L=3)
+img_gray = test(img1=de(img=img_gray, L=1) ,img2=img_gray)#de(img=img_gray, L=10))
 # img_gray[200:350, 200:250] = 0
 
 img_gray = img_gray.astype(np.uint8)
